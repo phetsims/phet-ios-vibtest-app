d51d5f3572ae1b9884b7d0470ecb5b4f27d71bc2
Jesse Greenberg <jesse.greenberg@colorado.edu>
2019-12-06 16:26:09 -0500

    Initial Commit

---
+++ phet-ios-vibtest-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  phet-ios-vibtest-app
+//
+//  Created by Jesse Greenberg on 12/6/19.
+//  Copyright © 2019 phet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
